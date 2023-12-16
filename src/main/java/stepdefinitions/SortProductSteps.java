d3d1b6dd993e2c2527d6d9c91937440cf8dcf4e1
nirbhay patil <nirbhaypatil@gmail.com>
2023-12-16 13:14:19 +0530

    adding scenario outline.

--- src/main/java/stepdefinitions/SortProductSteps.java
+++ src/main/java/stepdefinitions/SortProductSteps.java
@@ -18,27 +18,7 @@ public class SortProductSteps {
         menu.hoverOn("MEN");
         menu.clickOnMenuItem("T-Shirts");
     }
-//    @When("product items are sorted by high to low")
-//    public void product_items_are_sorted_by_high_to_low() {
-//        SearchResultsPage searchResults = new SearchResultsPage();
-//        searchResults.sortByPrices("hightolow");
-//    }
-//    @Then("tshirts should be displayed in ascending order by prices")
-//    public void tshirts_should_be_displayed_in_ascending_order_by_prices() {
-//        SearchResultsPage searchResults = new SearchResultsPage();
-//        List<Integer> prices =  searchResults.getPrices();
-//        SoftAssert softly = new SoftAssert();
-//        for( int i =0 ; i< prices.size()-1;i++ ){
-//            softly.assertTrue(prices.get(i).compareTo(prices.get(i+1)) <= 0,"Prices not in high to low order.");
-//        }
-//        softly.assertAll();
-//    }
-//
-//    @When("product items are sorted by low to high")
-//    public void product_items_are_sorted_by_low_to_high() {
-//        SearchResultsPage searchResults = new SearchResultsPage();
-//        searchResults.sortByPrices("lowtohigh");
-//    }
+    
     @Then("tshirts should be displayed in {string} order by prices")
     public void tshirts_should_be_displayed_in_descending_order_by_prices(String order) {
         System.out.println(order);
--- src/test/resources/sort-product-by-pricces.feature
+++ src/test/resources/sort-product-by-pricces.feature
@@ -1,7 +1,6 @@
 Feature:  Verify product item can be sorted in ascending or descending order
 
-  @wip
-  Scenario Outline: Verify if a product can be added without selecting size
+  Scenario Outline: Verify if list of products can be sorted
     Given User is on mens tshirt page
     When product items are sorted by "<criteria>"
     Then tshirts should be displayed in "<order>" order by prices
